5c1d0bc34a0668f5dee766d027ecf675c071b7b1
dalang <gxdong@ctrip.com>
2014-04-03 22:05:05 +0800

    add theme for putty

--- initial.sh
+++ initial.sh
@@ -13,3 +13,14 @@ ln -s ~/.vim/vimrc ~/.vimrc
 # tmux
 if [ -f ~/.tmux.conf ]; then mv ~/.tmux.conf ~/.tmux.conf.old; fi
 ln -s ~/dalang-Unix-like-Setting/.tmux.conf ~/.tmux.conf
+
+# oh-my-zsh
+if [ -f ~/.oh-my-zsh ]; then mv ~/.oh-my-zsh ~/.oh-my-zsh.old; fi
+ln -s ~/dalang-Unix-like-Setting/oh-my-zsh ~/.oh-my-zsh
+
+if [ ! -f ~/.oh-my-zsh/themes/dalang.zsh-theme ]; then ln -s ~/dalang-Unix-like-Setting/dalang.zsh-theme ~/.oh-my-zsh/themes/dalang.zsh-theme; fi
+
+if [ ! -f ~/.oh-my-zsh/plugins/razor ]; then ln -s ~/dalang-Unix-like-Setting/oh-my-zsh_razor_plugin ~/.oh-my-zsh/plugins/razor; fi
+
+if [ -f ~/.zshrc ]; then mv ~/.zshrc ~/.zshrc.old; fi
+ln -s ~/dalang-Unix-like-Setting/.zshrc ~/.zshrc
