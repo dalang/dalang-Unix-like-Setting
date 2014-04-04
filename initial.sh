f46aacace88ef6664c3e74e4638d2fb0b3e8dbe5
dalang <gxdong@ctrip.com>
2014-04-04 11:54:40 +0800

    fix bug of test in directory exist

--- initial.sh
+++ initial.sh
@@ -6,6 +6,8 @@
 cd dalang-vim
 ./update.sh
 cd -
+
+if [ -d ~/.vim ]; then mv ~/.vim ~/.vim.old; fi
 ln -s ~/dalang-Unix-like-Setting/dalang-vim ~/.vim
 if [ -f ~/.vimrc ]; then mv ~/.vimrc ~/.vimrc.old; fi
 ln -s ~/.vim/vimrc ~/.vimrc
@@ -15,11 +17,11 @@ if [ -f ~/.tmux.conf ]; then mv ~/.tmux.conf ~/.tmux.conf.old; fi
 ln -s ~/dalang-Unix-like-Setting/tmux.conf ~/.tmux.conf
 
 # oh-my-zsh
-if [ -f ~/.oh-my-zsh ]; then mv ~/.oh-my-zsh ~/.oh-my-zsh.old; fi
+if [ -d ~/.oh-my-zsh ]; then mv ~/.oh-my-zsh ~/.oh-my-zsh.old; fi
 ln -s ~/dalang-Unix-like-Setting/oh-my-zsh ~/.oh-my-zsh
 
 if [ ! -f ~/.oh-my-zsh/themes/dalang.zsh-theme ]; then ln -s ~/dalang-Unix-like-Setting/dalang.zsh-theme ~/.oh-my-zsh/themes/dalang.zsh-theme; fi
-if [ ! -f ~/.oh-my-zsh/plugins/razor ]; then ln -s ~/dalang-Unix-like-Setting/oh-my-zsh_razor_plugin ~/.oh-my-zsh/plugins/razor; fi
+if [ ! -d ~/.oh-my-zsh/plugins/razor ]; then ln -s ~/dalang-Unix-like-Setting/oh-my-zsh_razor_plugin ~/.oh-my-zsh/plugins/razor; fi
 
 if [ -f ~/.zshrc ]; then mv ~/.zshrc ~/.zshrc.old; fi
 ln -s ~/dalang-Unix-like-Setting/zshrc ~/.zshrc
